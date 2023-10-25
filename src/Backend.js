884958cf58876de5139daf7f59eb9ceeffc48ba6
Jacqueline <jacqueline.cai2004@gmail.com>
2023-10-25 18:00:19 -0400

    Adding documents into database

--- src/Backend.js
+++ src/Backend.js
@@ -12,12 +12,38 @@ async function main() {
 
 async function run() {
   try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    // const database = client.db('sample_mflix');
+    // const movies = database.collection('comments');
+    // // Query for a comment that has the name 'Taylor Scott'
+    // const query = { name: 'Taylor Scott' };
+    // const movie = await movies.findOne(query);
+    // console.log(movie);
+
+    const db = client.db('cu-supplies');
+    const sellers = db.collection('sellers');
+
+    let sellerDocument = {
+      "name": "Michael Clarkson",
+      "email": "michael.clarkson@cornell.edu",
+      "products": ["iClicker", "camels"]
+    }
+
+    const p = await sellers.insertOne(sellerDocument);
+
+    const filter = { "name": "Michael Clarkson" };
+    const document = await sellers.findOne(filter);
+    console.log(document);
+
+    // app.get('/seller', (req, res) => {
+    //   res.send(sellers.find())
+    // })
+
+    app.get('/seller/clarkson', (req, res) => {
+      res.send(JSON.stringify(document))
+    })
+
+  } catch (err) {
+    console.log(err.stack);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -25,10 +51,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.get('/update', (req, res) => {
+  res.send('update!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
