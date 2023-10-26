1aa85f16434c52c7395964803e07a4cb568bd67a
Vivian Zhou <87792254+vivianzhou1288@users.noreply.github.com>
2023-10-25 20:12:46 -0400

    Finished NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import {CgProfile} from 'react-icons/cg'
+import {BsFillHeartFill} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
     <div className = "px-5 py-5 bg-[#B31B1B] flex flex-row items-center justify-between">
       <h1 className = "text-xl text-white">CU Supplies</h1>
-      <SearchBar />
+      <SearchBar/>
+      <div className = "flex flex-row items-center justify-between ">
+        <CgProfile size = {35} className = "mr-2"/>
+        <h1 className = "mr-2 text-lg">Vivian</h1>
+        <BsFillHeartFill size = {30}/>
+      </div>
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import {FaSearch} from 'react-icons/fa'
+
 const SearchBar = () => {
   return (
-    <div>
-      <h3>SearchBar</h3>
+    <div className = "bg-white flex flex-row items-center rounded-xl h-10 w-3/6 mr-3">
+      <FaSearch id = "search-icon" className = "mx-2"/>
+      <input placeholder='Search' className="border-none w-[63%] bg-transparent outline-none"/>
     </div>
   )
 }
